db0883dd8788262b186c5c0ea7de141ee087f0f9
YouMe6110 <ume0116@naver.com>
2022-02-22 00:18:50 +0900

    React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAK6NRniZEfrV2GJVRrF01O96-4LU9n53w",
+    authDomain: "nwitter-22210.firebaseapp.com",
+    projectId: "nwitter-22210",
+    storageBucket: "nwitter-22210.appspot.com",
+    messagingSenderId: "523742206029",
+    appId: "1:523742206029:web:a8fb98e0eb8d2a4a0d6404"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
